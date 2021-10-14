9a2926b561760c1326bfb264f50ddf0a1ff8371d
Michel La Guardia <mlaguardia@gmail.com>
2021-10-14 18:50:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ const mappingKeysObj = (obj) => {
   } else {
     let key, keys = Object.keys(obj);
     let n = keys.length;
+
     for (let i=0; i<n; i++) {
       key = keys[i];
       if (typeof obj[key] === 'object' && obj[key] !== null) {
         newObj[key.toLowerCase()] = callBackKeys(obj[key]);
       } else if (obj[key] instanceof Array) {
         let itens = [];
+ 
         for (const item of obj[key]) {
           itens.push(mappingKeysObj(item));
         }
 
-         newObj[key.toLowerCase()] = itens;
+        newObj[key.toLowerCase()] = itens;
       } else {
         newObj[key.toLowerCase()] = obj[key];
       }
@@ -31,6 +33,7 @@ const mappingKeysObj = (obj) => {
 
 const callBackKeys = (obj) => {
   let newObj;
+
   if (obj instanceof Array) {
   	newObj = [];
     for (const item of obj) {
@@ -46,5 +49,6 @@ const callBackKeys = (obj) => {
 
 export const convertAllKeysToLower = (obj) => {
   const newObj = callBackKeys(obj);
+
   return newObj;
 };
