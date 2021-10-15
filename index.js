e8609f3d7b62a19edc7152dece5fffb3c980594f
Michel La Guardia <mlaguardia@gmail.com>
2021-10-15 10:43:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,26 +10,32 @@ const getArrayKeysToLowerCase = (obj) => {
   return arr;
 };
 
+const setObjectKeysToLowerCase = (obj, newObj) => {
+  let key, keys = Object.keys(obj);
+  let n = keys.length;
+
+  for (let i=0; i<n; i++) {
+    key = keys[i];
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      newObj[key.toLowerCase()] = setAllKeysToLowerCase(obj[key]);
+    } else if (obj[key] instanceof Array) {
+      const itens = getArrayKeysToLowerCase(obj[key]);
+      newObj[key.toLowerCase()] = itens;
+    } else {
+      newObj[key.toLowerCase()] = obj[key];
+    }
+  }
+
+  return newObj;
+};
+
 const setAllKeysToLowerCase = (obj) => {
   let newObj = {};
 
   if (obj instanceof Array) {
     newObj = getArrayKeysToLowerCase(obj);
   } else {
-    let key, keys = Object.keys(obj);
-    let n = keys.length;
-
-    for (let i=0; i<n; i++) {
-      key = keys[i];
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-        newObj[key.toLowerCase()] = setAllKeysToLowerCase(obj[key]);
-      } else if (obj[key] instanceof Array) {
-        const itens = getArrayKeysToLowerCase(obj[key]);
-        newObj[key.toLowerCase()] = itens;
-      } else {
-        newObj[key.toLowerCase()] = obj[key];
-      }
-    }
+    newObj = setObjectKeysToLowerCase(obj, newObj);
   }
 
   return newObj;
